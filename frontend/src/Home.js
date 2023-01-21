8bc3b7362dff4eba0e3d8b3f9fa159cb5bca2146
Chernandez2022 <hcarlos0816@gmail.com>
2023-01-21 00:14:41 -0500

    styled home and forms

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,19 +2,25 @@ function Home() {
   return (
     <main>
       <h1>UNITY</h1>
-      <div>
+      {/* <div>
         <img
           className="home"
           height="300"
           width="500"
-          src={process.env.REACT_APP_SERVER_URL + "images/chia-fruit-drink.jpg"}
+          src={process.env.REACT_APP_SERVER_URL + "images/logo.png"}
           alt="Chia Fruit Shake"
         />
         <div>
           Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on{" "}
           <a href="UNSPLASH_LINK">Unsplash</a>
         </div>
+      </div> */}
+      <div class="wrapper">
+        <div class="box box2">
+          This box has a background color with an alpha channel
+        </div>
       </div>
+
       <a href="/places">
         <button className="btn-primary">Places Page</button>
       </a>
--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -9,16 +9,13 @@ function Navigation() {
 
   let loginActions = (
     <>
-      <li style={{ float: "right" }}>
-        <a href="#" onClick={() => history.push("/sign-up")}>
-          Sign Up
-        </a>
-      </li>
-      <li style={{ float: "right" }}>
-        <a href="#" onClick={() => history.push("/login")}>
-          Login
-        </a>
-      </li>
+      <a href="#" onClick={() => history.push("/sign-up")}>
+        Sign Up
+      </a>
+
+      <a href="#" onClick={() => history.push("/login")}>
+        Login
+      </a>
     </>
   );
 
@@ -33,25 +30,20 @@ function Navigation() {
   return (
     <details>
       <summary></summary>
-      <nav>
-        <ul>
-          <li>
-            <a class="active" href="#" onClick={() => history.push("/")}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" onClick={() => history.push("/places")}>
-              Places
-            </a>
-          </li>
-          <li>
-            <a href="#" onClick={() => history.push("/places/new")}>
-              Add Place
-            </a>
-          </li>
-          {loginActions}
-        </ul>
+      <nav className="menu">
+        <a class="active" href="#" onClick={() => history.push("/")}>
+          Home
+        </a>
+
+        <a href="#" onClick={() => history.push("/places")}>
+          Places
+        </a>
+
+        <a href="#" onClick={() => history.push("/places/new")}>
+          Add Place
+        </a>
+
+        {loginActions}
       </nav>
     </details>
   );
