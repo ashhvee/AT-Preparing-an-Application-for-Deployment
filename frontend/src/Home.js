dc54f1b3bc75063988175507a43f9b41c789b131
Ashley Vieira <ash.adr24212@gmail.com>
2023-01-20 22:20:47 -0500

    styling

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,16 +1,14 @@
-
 function Home() {
   return (
     <main>
       <h1>UNITY</h1>
       <div >
-        <img className="home" height="300" width="500" src={process.env.REACT_APP_SERVER_URL + 'images/chia-fruit-drink.jpg'} alt="Chia Fruit Shake" />
-        <div className="home">
-          Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
-        </div>
+        <img className="home" src={process.env.REACT_APP_SERVER_URL + 'images/hotelBackground.jpg'} alt="background" />
+        <div class="wrapper">
+    <div class="box box2">This box has a background color with an alpha channel</div>
+</div>
       </div>
       <a href="/places">
-        <button className="btn-primary">Places Page</button>
       </a>
     </main>
   );
